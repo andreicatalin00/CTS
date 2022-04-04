3c781f8becaec5a33e0e5f44ea54e409a1eaf794
catalin <militarescuandrei19@stud.ase.ro>
2022-04-04 22:49:12 +0200

    Added the custom Exception ValueException

--- assignment_2/src/com/ase/exceptions/ValueException.java
+++ assignment_2/src/com/ase/exceptions/ValueException.java
@@ -2,6 +2,6 @@ package com.ase.exceptions;
 
 public class ValueException extends Exception{
     public ValueException(){
-        super("This is a value error");
+        super("The value is negative!");
     }
 }
